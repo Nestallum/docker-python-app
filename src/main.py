5679eff2c2a95fce0d14be4907cb4ab2ae253ff0
Nestallum <nassimlattab@gmail.com>
2024-04-08 10:59:57 +0200

    serveur web python

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
+from flask import Flask
 import pandas as pd
-import numpy as np
 
-def main():
+app = Flask(__name__)
 
-    # Création du dataframe
+def generate_html_from_dataframe(df):
+    html = "<h1>Données des étudiants</h1>"
+    html += df.to_html(index=False)
+    return html
+
+@app.route('/')
+def index():
     data = {
         'Nom': ['LATTAB', 'AZZAOUI'],
         'Prénom': ['Nassim', 'Mohamed'],
         'Numéro étudiant': ['123456789', '987654321']
     }
-
     df = pd.DataFrame(data)
+    return generate_html_from_dataframe(df)
 
-    # Affichage du dataframe
-    print(df)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
